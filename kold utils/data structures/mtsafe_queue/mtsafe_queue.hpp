1c6eab3c4c5468acc12472f437b84dfbd9a10446
shonginger <58817276+shonginger@users.noreply.github.com>
2020-01-21 16:52:15 +0200

    waitqueue finished need to be tested
    test case 1: single thread, simply see that queue functions as queue (push works, pop works (for different types), timedpop works(for different types))
    test case 2: fill queue with different tasks (numbers as of right now, waitqueue ), using 2 threads pull out items (can be int for tests)
    test case 3: same as test case 3 use atleast 10 threads.

--- kold utils/data structures/mtsafe_queue/mtsafe_queue.hpp
+++ kold utils/data structures/mtsafe_queue/mtsafe_queue.hpp
@@ -1,29 +1,89 @@
 #ifndef KOLD_DB_WAIT_QUEUE
 #define KOLD_DB_WAIT_QUEUE
 
-#include <queue>
 #include <chrono>
+#include <thread>
+#include <mutex>
+#include <condition_variable>
 
 // to be implmented #include "task.hpp"
 typedef int Task;
 
+template <class ADTQueue>
 class WaitQueue
 {
 public:
   WaitQueue();
-  ~WaitQUeue();
+  ~WaitQueue();
 
- Task Push(const Task& task);
- Task& Pop();
- Task& TimedPop(std::chrono::miliseconds timeout);
+ void Push(const Task& task);
+ 
+ template <class typeof> typeof Pop();
+ template <class typeof> typeof TimedPop(std::chrono::nanoseconds timeout);
   
 private:
    WaitQueue(const WaitQueue& other);
    WaitQueue& operator=(const WaitQueue& other);
   
-  std::queue<Task> m_queue;
+  ADTQueue m_queue;
   std::mutex m_lock;
-  std::condition_variable m_queueNotEmpty;
+  std::condition_variable m_queue_not_empty;
+};
+
+template <class Q>
+WaitQueue<Q>::WaitQueue()
+  : m_queue()
+  , m_lock()
+  , m_queue_not_empty()
+{}
+
+template <class Q>
+WaitQueue<Q>::~WaitQueue(){}
+
+template <class Q>
+void WaitQueue<Q>::Push(const Task& task)
+{
+  std::unique_lock<std::mutex> scope_lock(m_lock);
+
+  m_queue.push(task);
+
+  m_queue_not_empty.notify_one();
+}
+
+template <class Q>
+template <class typeof>
+typeof WaitQueue<Q>::Pop()
+{
+    std::unique_lock<std::mutex> scope_lock(m_lock);
+
+    while (m_queue.empty())
+    {
+        m_queue_not_empty.wait(m_lock);
+    }
+    
+    typeof ret_val = m_queue.front();
+    m_queue.pop();
+    
+    return ret_val;
+}
+
+template <class Q>
+template <class typeof>
+typeof WaitQueue<Q>::TimedPop(std::chrono::nanoseconds timeout)
+{
+    std::unique_lock<std::mutex> scope_lock(m_lock);
+
+    while (m_queue.empty())
+    {
+        m_queue_not_empty.wait_for(std::chrono::duration_cast<std::chrono::nanoseconds>(timeout), m_lock);
+    }
+    
+    typeof ret_val = m_queue.front();
+    m_queue.pop();
+    
+    return ret_val;
 }
 
 #endif // KOLD_DB_WAIT_QUEUE
+
+
