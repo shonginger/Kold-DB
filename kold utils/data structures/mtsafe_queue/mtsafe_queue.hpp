3f5e9ffdff60cf7ac45ee64494bb98b420629db5
shonginger <58817276+shonginger@users.noreply.github.com>
2020-01-21 11:28:11 +0200

    Update mtsafe_queue.hpp

--- kold utils/data structures/mtsafe_queue/mtsafe_queue.hpp
+++ kold utils/data structures/mtsafe_queue/mtsafe_queue.hpp
@@ -1,17 +1,17 @@
 #ifndef KOLD_DB_WAIT_QUEUE
 #define KOLD_DB_WAIT_QUEUE
 
-#include <queue>
 #include <chrono>
 
 // to be implmented #include "task.hpp"
 typedef int Task;
 
+template <class QueueType>
 class WaitQueue
 {
 public:
   WaitQueue();
-  ~WaitQUeue();
+  ~WaitQueue();
 
  Task Push(const Task& task);
  Task& Pop();
@@ -21,9 +21,21 @@ private:
    WaitQueue(const WaitQueue& other);
    WaitQueue& operator=(const WaitQueue& other);
   
-  std::queue<Task> m_queue;
+  QueueType<Task> m_queue;
   std::mutex m_lock;
-  std::condition_variable m_queueNotEmpty;
+  std::condition_variable m_queue_not_empty;
 }
+
+template <class Q>
+WaitQueue<Q>::WaitQueue<Q>()
+  : m_queue()
+  , m_lock()
+  , m_queue_not_empty()
+{}
+
+template <class Q>
+WaitQueue<Q>:~WaitQueue<Q>{}
+
+
 
 #endif // KOLD_DB_WAIT_QUEUE
